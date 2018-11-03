d402ddbcb09ff386051b25b5969cd40493b97c03
gk <gim.kobayashi@gmail.com>
2018-11-04 00:09:39 +0900

    update

--- stream.py
+++ stream.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, print_function
-
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -13,33 +12,33 @@ access_token=os.environ['access_token']
 access_token_secret=os.environ['access_token_secret']
 
 class StdOutListener(StreamListener):
-		""" A listener handles tweets that are received from the stream.
-		This is a basic listener that just prints received tweets to stdout.
-		"""
-		def on_data(self, data):
-				obj = json.loads(data)
-				serialized = json.dumps(obj, indent=2, ensure_ascii=False)
-				hashed = sha256(bytes(serialized, 'utf8')).hexdigest()
-				open(f'jsons/{hashed}', 'w').write( serialized )
-				print(serialized)
-				return True
+	""" A listener handles tweets that are received from the stream.
+	This is a basic listener that just prints received tweets to stdout.
+	"""
+	def on_data(self, data):
+			obj = json.loads(data)
+			serialized = json.dumps(obj, indent=2, ensure_ascii=False)
+			hashed = sha256(bytes(serialized, 'utf8')).hexdigest()
+			open(f'jsons/{hashed}', 'w').write( serialized )
+			print(serialized)
+			return True
 
-		def on_error(self, status):
-				print(status)
+	def on_error(self, status):
+			print(status)
 
 def random_sample():
-		l = StdOutListener()
-		auth = OAuthHandler(consumer_key, consumer_secret)
-		auth.set_access_token(access_token, access_token_secret)
-		stream = Stream(auth, l)
-		stream.sample(languages=['ja'])	
+	l = StdOutListener()
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	stream = Stream(auth, l)
+	stream.sample(languages=['ja'])	
 
 def filter_words():
-		l = StdOutListener()
-		auth = OAuthHandler(consumer_key, consumer_secret)
-		auth.set_access_token(access_token, access_token_secret)
-		stream = Stream(auth, l)
-		stream.filter(languages=["ja"], track=['ベイスターズ', 'FGO', '艦これ', 'パズドラ', 'モンスト', 'オセロニア'])
+	l = StdOutListener()
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	stream = Stream(auth, l)
+	stream.filter(languages=["ja"], track=['グラブル', 'ベイスターズ', 'FGO', '艦これ', 'パズドラ', 'モンスト', 'オセロニア'])
 
 def rap(func):
 	func()
@@ -49,4 +48,4 @@ def main():
 	with PPE(max_workers=2) as exe:
 		exe.map(rap, [random_sample, filter_words])
 if __name__ == '__main__':
-  main()
+	main()
